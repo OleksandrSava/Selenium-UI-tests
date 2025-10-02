ef078ac56577a56a02c124e2f61f72c02bbf81b5
Oleksandr Sava <sasha.pop.2019@gmail.com>
2025-10-02 15:51:54 +0200

    driver fix

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -8,11 +8,12 @@ from Config.Data import Data
 def driver(request):
 
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
     options.add_argument("--window-size=1920,1080")
-
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
 
