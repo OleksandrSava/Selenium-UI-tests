4401dac44775a4f3b067a89616014049a2ce3bbc
Oleksandr Sava <sasha.pop.2019@gmail.com>
2025-10-03 12:09:25 +0200

    Conftest fix

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -22,6 +22,25 @@ def driver(request):
     driver.quit()
 
 
+import pytest
+import allure
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name=f"Failure_{item.name}",
+                attachment_type=allure.attachment_type.PNG
+            )
+
+
+
 @pytest.fixture
 def prepare_login(request):
 
